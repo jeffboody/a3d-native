fe0f05db319c5cac4e9217e5a47eafa1409c4e46
Jeff Boody <jeffboody@gmail.com>
2013-04-01 03:25:27 +0100

    add support for LOAX

--- a3d_GL.h
+++ a3d_GL.h
@@ -26,6 +26,9 @@
 
 #if defined(A3D_GLESv1_CM)
 	#include <GLES/gl.h>
+#elif defined(A3D_GLESv2_LOAX)
+	#define A3D_GLESv2
+	#include <loax/gl2.h>
 #elif defined(A3D_GLESv2)
 	#include <GLES2/gl2.h>
 #elif defined(A3D_GL2)

fc1ce710cf532ebcd9e3e04fa878213130b906cc
Jeff Boody <jeffboody@gmail.com>
2013-11-12 20:17:16 +0000

    add a3d_fplane_t for frustum planes

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_CFLAGS    := -Wall -D$(A3D_CLIENT_VERSION)
 LOCAL_SRC_FILES := a3d/a3d_log.c a3d/a3d_glsm.c a3d/a3d_time.c a3d/a3d_list.c \
                    a3d/a3d_tex.c a3d/a3d_texfont.c a3d/a3d_texstring.c a3d/a3d_workq.c \
                    a3d/math/a3d_mat3f.c a3d/math/a3d_mat4f.c a3d/math/a3d_stack4f.c a3d/math/a3d_regionf.c a3d/math/a3d_vec3f.c a3d/math/a3d_vec4f.c \
-                   a3d/math/a3d_quaternion.c a3d/math/a3d_orientation.c a3d/math/a3d_sphere.c a3d/math/a3d_plane.c a3d/a3d_GL.c
+                   a3d/math/a3d_quaternion.c a3d/math/a3d_orientation.c a3d/math/a3d_sphere.c a3d/math/a3d_plane.c a3d/math/a3d_fplane.c a3d/a3d_GL.c
 
 ifeq ($(A3D_CLIENT_VERSION),A3D_GLESv1_CM)
 	LOCAL_SRC_FILES := $(LOCAL_SRC_FILES) a3d/a3d_GLESv1_CM.c
